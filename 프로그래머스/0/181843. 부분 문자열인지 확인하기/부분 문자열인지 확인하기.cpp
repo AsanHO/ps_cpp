5d1bcaecf4d40aae5e2dc6ef4629bcfa175bc7e1
eunho <85553431+AsanHO@users.noreply.github.com>
2024-07-15 08:13:41 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(string my_string, string target) {
+    int answer = 1;
+    if (my_string.find(target)==string::npos){
+answer = 0;
+}
+    return answer;
+}
