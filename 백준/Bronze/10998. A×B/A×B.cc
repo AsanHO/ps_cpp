c696b56fafb871d046133ed2046fb273d3c50fd9
eunho <85553431+AsanHO@users.noreply.github.com>
2024-11-19 14:47:16 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int A;
+int B;
+
+int main(int argc, char const *argv[]) {
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    cin >> A;
+    cin >> B;
+    cout << A * B;
+
+    return 0;
+}
