8d5d1ca66ee5753a5d59070b7cf033f3a12d2df9
eunho <85553431+AsanHO@users.noreply.github.com>
2025-05-23 14:59:05 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 428 ms, Memory: 15988 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, cnt;
+int arr[1000001], dp[1000001];
+vector<int> v, ans;
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    v.push_back(arr[0]);
+
+    for (int i = 1; i < N; i++) {
+        if (v[cnt] < arr[i]) {
+            v.push_back(arr[i]);
+            cnt++;
+        } else {
+            // 같거나 작은 위치를 lower_bound로 찾음
+            int pos = lower_bound(v.begin(), v.end(), arr[i]) - v.begin();
+            v[pos] = arr[i];
+        }
+    }
+
+    cout << v.size() << endl;
+    // for (int i : v) {
+    //     cout << i << " ";
+    // }
+}
