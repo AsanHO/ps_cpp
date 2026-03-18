cc2c9a77edcc9ad041e631eaaf6ae221711332c0
eunho <85553431+AsanHO@users.noreply.github.com>
2026-03-18 17:15:02 +0900

    [Platinum V] Title: 숨바꼭질 5, Time: 12 ms, Memory: 6060 KB -BaekjoonHub

--- 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
+++ 백준/Platinum/17071. 숨바꼭질 5/숨바꼭질 5.cc
@@ -1,40 +1,60 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
-const int max_n = 500000; 
-int visited[2][max_n + 4], a, b, ok, turn = 1;
+
+/*
+이전과 동일하나, 홀짝을 구분해줘야 한다.
+수빈 동생
+5초 7초인경우
+수빈이 왔다 갔다 해서 만날 수 있음.
+*/
+int N, K;
+int visited[2][500004];
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    cin >> a >> b;
-    if(a == b){cout << 0 << "\n"; return 0;}
+    cin >> N >> K;
+    if (N == K) {
+        cout << 0 << "\n";
+        return 0;
+    }
     queue<int> q;
-    visited[0][a] = 1;
-    q.push(a);
-    while(q.size()){
-        b += turn;
-        if(b > max_n)break;  
-        if(visited[turn % 2][b]) {
+    visited[0][N] = 1;
+    q.push(N);
+    int turn = 1;
+    int ok = 0;
+    while (q.size()) {
+        K += turn;
+        if (K > 500000) break;
+        if (visited[turn % 2][K]) {
             ok = true;
             break;
         }
         int qSize = q.size();
-        for(int i = 0; i < qSize; i++){
-            int x = q.front(); q.pop();
-			for(int nx : {x + 1, x - 1, x * 2}){
-                if(nx < 0 || nx > max_n || visited[turn % 2][nx]) continue;
-                visited[turn % 2][nx] = visited[(turn + 1) % 2][x] + 1; 
-                if(nx == b){
-                    ok = 1; break;
+        for (int i = 0; i < qSize; i++) {
+            int x = q.front();
+            q.pop();
+            for (int nx : {x + 1, x - 1, x * 2}) {
+                if (nx < 0 || nx > 500000 || visited[turn % 2][nx]) continue;
+                visited[turn % 2][nx] = visited[(turn + 1) % 2][x] + 1;
+                if (nx == K) {
+                    ok = 1;
+                    break;
                 }
-                q.push(nx); 
-			} 
-			if(ok)break;
+                q.push(nx);
+            }
+            if (ok) break;
         }
-        if(ok)break;
+        if (ok) break;
         turn++;
     }
-    if(ok) cout << turn << "\n";
-    else cout << -1 << "\n";
+    if (ok)
+        cout << turn;
+    else
+        cout << -1;
     return 0;
 }
