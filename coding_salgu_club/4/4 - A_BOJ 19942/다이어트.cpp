62a2dfc0cf7f06475468a9674949d44610244ab8
AsanHO <gorma00@naver.com>
2026-06-25 15:27:35 +0900

    udt : 다이어트 비트마스킹

--- coding_salgu_club/4/4 - A_BOJ 19942/다이어트.cpp
+++ coding_salgu_club/4/4 - A_BOJ 19942/다이어트.cpp
@@ -1,84 +1,59 @@
 #include <algorithm>
+#include <bitset>
 #include <iostream>
-/*
-6
-100 70 90 10
-30 55 10 8 100
-60 10 10 2 70
-10 80 50 0 50
-40 30 30 8 60
-60 10 70 2 120
-20 70 50 4 4
-*/
+#include <vector>
 using namespace std;
 int N;
-int min_targets[4];
-int items[15][5];
-int item_visited[15];
 int min_result = 2100000000;
-int final_items[15];
+int min_nutritions[4];
+int nutritions[15][5];
 
 // 현재까지 1인 아이템들의 영양합과 min_targets비교
-void check_item() {
-    int sum[5] = {0};
-    for (int i = 0; i < N; i++) {
-        if (!item_visited[i]) continue;
-        cout << i << " + ";
-        for (int j = 0; j < 5; j++) {
-            sum[j] += items[i][j];
-        }
-    }
-    cout << endl;
-    for (int i = 0; i < 4; i++) {
-        if (min_targets[i] > sum[i]) {
-            return;
-        }
-    }
-    if (min_result > sum[4]) {
-        min_result = sum[4];
-        for (int i = 0; i < N; i++) {
-            if (item_visited[i]) {
-                final_items[i] = 1;
-            } else {
-                final_items[i] = 0;
-            }
-        }
-    }
-}
-void back_tracking(int cur) {
-    for (int i = cur + 1; i < N; i++) {
-        if (item_visited[i]) continue;
-        item_visited[i] = 1;
-        check_item();
-        back_tracking(i);
-        item_visited[i] = 0;
-    }
-}
+vector<int> result;
 int main() {
     cin >> N;
     for (int i = 0; i < 4; i++) {
-        cin >> min_targets[i];
+        cin >> min_nutritions[i];
     }
-
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < 5; j++) {
-            cin >> items[i][j];
+            cin >> nutritions[i][j];
         }
     }
+    // 6인경우 1부터 63까지
+    for (int mask = 1; mask < (1 << N); mask++) {
+        int p = 0;
+        int f = 0;
+        int s = 0;
+        int v = 0;
+        int cost = 0;
+        vector<int> temp;
+        for (int i = 0; i < N; i++) {
+            if (mask & (1 << i)) {  // 켜져 있으면 추가
+                p += nutritions[i][0];
+                f += nutritions[i][1];
+                s += nutritions[i][2];
+                v += nutritions[i][3];
+                cost += nutritions[i][4];
+                temp.push_back(i);
+            }
+        }
 
-    for (int i = 0; i < N; i++) {
-        item_visited[i] = 1;
-        check_item();
-        back_tracking(i);
-        item_visited[i] = 0;
+        if (p >= min_nutritions[0] && f >= min_nutritions[1] && s >= min_nutritions[2] && v >= min_nutritions[3]) {
+            if (min_result > cost) {
+                min_result = cost;
+                result = temp;
+            }
+        }
     }
     if (min_result == 2100000000) {
         cout << -1;
         return 0;
     }
-    cout << min_result << endl;
-    for (int i = 0; i < N; i++) {
-        if (final_items[i]) cout << i + 1 << " ";
+    cout << min_result << "\n";
+    for (int num : result) {
+        cout << num + 1 << " ";
     }
+
     return 0;
 }
